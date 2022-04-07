dc85d36e4ac99a90300126ec16660196f63b8f83
DoojinBaek <djinb1127@gmail.com>
2022-04-07 16:29:35 +0900

    TFBP directory

--- MTL_hyperparameter_tuning.py
+++ MTL_hyperparameter_tuning.py
@@ -9,8 +9,8 @@ import pandas as pd
 import torch.nn.functional as F
 
 from sklearn import metrics
-from DeepBind import datasets, dataset_loader, test_dataset_loader
-from DeepBind import  ConvNet_test
+from TFBP import datasets, dataset_loader, test_dataset_loader
+from TFBP import  ConvNet_test
 
 if(torch.cuda.is_available()):
     print('Torch',torch.__version__, 'is available')
